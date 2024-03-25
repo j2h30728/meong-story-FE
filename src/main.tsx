411183d8f7a9d968c60095d249fa0180405561ba
Dam <60846068+j2h30728@users.noreply.github.com>
2024-03-26 02:24:24 +0900

    feat: style theme 생성[#3]

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 import router from "./router/index.tsx";
+import { ThemeProvider } from "styled-components";
+import { THEME } from "./styles/theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={THEME}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
