eab8597fb62d3f6455845e341e925a98578707bc
Dam <60846068+j2h30728@users.noreply.github.com>
2024-03-26 14:59:06 +0900

    feat: route path 변경 및 추가[#5]

--- src/router/constants.ts
+++ src/router/constants.ts
@@ -2,13 +2,15 @@ const ROUTE_PATH = {
   ROOT: "/",
   HOME: "",
   INTRO: "intro",
-  LOGIN: "login",
-  REGISTER_DOG: "register/dog",
-  REGISTER_PEOPLE: "register/people",
-  VERIFICATION: "verification/:type",
-  VERIFICATION_UPLOAD: "verification/:type/upload",
-  SLIDES: "slides",
-  SETTING: "setting",
+  LOGIN: "/login",
+  REGISTER_DOG: "/register/dog",
+  REGISTER_PEOPLE: "/register/people",
+  VERIFICATION: "/verification/:type",
+  VERIFICATION_UPLOAD: "/verification/:type/upload",
+  SLIDE: "/slide",
+  GRID: "/grid",
+  SETTING: "/setting",
+  CALENDAR: "/calendar",
 } as const;
 
 export default ROUTE_PATH;
