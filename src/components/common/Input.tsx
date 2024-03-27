6c3f1a2cf3b5d375702768f7c4bb1f64a2aa7b74
Dam <60846068+j2h30728@users.noreply.github.com>
2024-03-27 20:51:51 +0900

    feat: input 컴포넌트 수정 [#5]

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,13 +1,29 @@
 import { InputHTMLAttributes } from "react";
 import styled from "styled-components";
+import { DoubleCheck } from "../Icons";
 
-const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
-  return <InputContainer {...props} />;
+const Input = ({
+  isValid,
+  ...props
+}: InputHTMLAttributes<HTMLInputElement> & { isValid: boolean }) => {
+  return (
+    <Container>
+      <InputItem {...props} />
+      <ValidDoubleCheck isValid={isValid} className="check" />
+    </Container>
+  );
 };
 
 export default Input;
-
-const InputContainer = styled.input`
+const Container = styled.div`
+  position: relative;
+  .check {
+    position: absolute;
+    right: 0;
+    top: 8px;
+  }
+`;
+const InputItem = styled.input`
   width: 273px;
   border-bottom: 1px solid black;
   padding: 4px 0;
@@ -20,4 +36,9 @@ const InputContainer = styled.input`
   &::placeholder {
     color: ${({ theme }) => theme.COLORS["INACTIVE-BUTTON"]};
   }
+`;
+
+const ValidDoubleCheck = styled(DoubleCheck)<{ isValid: boolean }>`
+  color: ${({ theme, isValid }) =>
+    isValid ? theme.COLORS["P-BUTTON1"] : theme.COLORS["INACTIVE-BUTTON"]};
 `;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
+export { default as Input } from "./common/Input";
+export { default as Button } from "./common/Button";
 export { default as Layout } from "./common/Layout";
