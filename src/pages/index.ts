1a6a4160063003465de1307931202c7bd95038de
Dam <60846068+j2h30728@users.noreply.github.com>
2024-03-30 13:09:52 +0900

    chore: 코드 모듈화 [#9]

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,17 +1,19 @@
-export { default as Register } from "./auth/Register";
+export { default as Register } from './auth/Register';
+export { default as KakaoLogIn } from './auth/KakaoLogIn';
 
-export { default as UploadWalk } from "./verification/UploadWalk";
-export { default as UploadBath } from "./verification/UploadBath";
-export { default as UploadMeal } from "./verification/UploadMeal";
-export { default as UploadSnack } from "./verification/UploadSnack";
-export { default as UploadEtc } from "./verification/UploadEtc";
-export { default as VerificationWalk } from "./verification/VerificationWalk";
+export { default as UploadWalk } from './verification/UploadWalk';
+export { default as UploadBath } from './verification/UploadBath';
+export { default as UploadMeal } from './verification/UploadMeal';
+export { default as UploadSnack } from './verification/UploadSnack';
+export { default as UploadEtc } from './verification/UploadEtc';
+export { default as VerificationWalk } from './verification/VerificationWalk';
 
-export { default as Intro } from "./Intro";
-export { default as Home } from "./Home";
+export { default as Intro } from './Intro';
+export { default as Home } from './Home';
 
-export { default as Slide } from "./Slide";
-export { default as Grid } from "./Grid";
-export { default as Calendar } from "./Calendar";
-export { default as Setting } from "./Setting";
-export { default as ErrorPage } from "./ErrorPage";
+export { default as Slide } from './Slide';
+export { default as Grid } from './Grid';
+export { default as GridByUser } from './GridByUser';
+export { default as Calendar } from './Calendar';
+export { default as Setting } from './Setting';
+export { default as ErrorPage } from './ErrorPage';
