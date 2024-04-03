fac8a4c4c355837635a2d49e6deeb54c55a67371
Dam <60846068+j2h30728@users.noreply.github.com>
2024-04-03 23:10:13 +0900

    feat: user image 컴포넌트 사이즈 추가 [#23]

--- src/components/common/UserImage.tsx
+++ src/components/common/UserImage.tsx
@@ -1,11 +1,12 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const STYLES = {
   SIZE: {
-    XS: "40px",
-    SM: "50px",
-    MD: "90px",
-    XL: "216px",
+    XXS: '30px',
+    XS: '40px',
+    SM: '50px',
+    MD: '90px',
+    XL: '216px',
   },
 } as const;
 
@@ -14,6 +15,7 @@ const StyledUserImage = styled.img<{ size: keyof typeof STYLES.SIZE }>`
   height: ${({ size }) => STYLES.SIZE[size]};
   background-color: gray;
   border-radius: 100%;
+  object-fit: cover;
 `;
 
 const UserImage = ({
