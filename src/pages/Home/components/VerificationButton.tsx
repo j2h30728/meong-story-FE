36bd22af529e6abf90603a065781203cf26c1040
Dam <60846068+j2h30728@users.noreply.github.com>
2024-04-09 02:21:30 +0900

    feat: home 페이지의 verification button 컴포넌트 분리 [#38]

---
+++ src/pages/Home/components/VerificationButton.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from 'react-router-dom';
+import * as S from '../Home.styled';
+import { ReactNode } from 'react';
+import { VERIFICATION } from '../../../utils/constants';
+
+const VerificationButton = ({
+  icon,
+  title,
+  name,
+  navigatePath,
+}: {
+  icon?: ReactNode;
+  title: string;
+  name: string;
+  navigatePath: string;
+}) => {
+  const navigate = useNavigate();
+  switch (name) {
+    case VERIFICATION.SURVIVAL:
+      return (
+        <S.VerificationSurvival onClick={() => navigate(navigatePath)}>
+          순간포착! 일상인증
+        </S.VerificationSurvival>
+      );
+    default:
+      return (
+        <S.VerificationItem id={name} onClick={() => navigate(navigatePath)}>
+          {icon}
+          <span>{title}</span>
+        </S.VerificationItem>
+      );
+  }
+};
+
+export default VerificationButton;
