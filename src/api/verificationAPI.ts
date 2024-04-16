6d2132e1230b76af0f96539c6b7682fb32edbf0f
Dam <60846068+j2h30728@users.noreply.github.com>
2024-04-17 02:02:40 +0900

    feat: upload pet verification api 호출 로직 생성 [#55]

--- src/api/verificationAPI.ts
+++ src/api/verificationAPI.ts
@@ -1,8 +1,11 @@
 import QueryString from 'qs';
 
 import apiClient from '.';
-import { VerificationCount } from '../types/verification';
 import { END_POINT } from '../constants/endPoint';
+import type {
+  UploadVerificationContents,
+  VerificationCount,
+} from '../types/verification';
 
 const verificationAPI = {
   /** verification count 및 pet info 조회 */
@@ -10,6 +13,14 @@ const verificationAPI = {
     const query = QueryString.stringify({ petId: petId }, { skipNulls: true });
     const { data } = await apiClient.get<VerificationCount>(
       `${END_POINT.HOME}?${query}`
+    );
+    return data;
+  },
+  /** pet verification post */
+  postVerification: async (body: UploadVerificationContents) => {
+    const { data } = await apiClient.post<UploadVerificationContents>(
+      END_POINT.POST,
+      body
     );
     return data;
   },
