19b2c023faea439216776a21152d522fabf8c973
j2h30728 <rachel2148072@gmail.com>
2024-11-25 13:29:31 +0000

    build: tanstack query devtools 설치[#38]

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,11 +1,15 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import queryClient from '@/shared/lib/query/queryClient';
 
 const QueryProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 };
 
